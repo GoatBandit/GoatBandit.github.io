e2e767e4264e5f148811da6e7eef9e16a17de587
GoatBandit <connoreaster@gmail.com>
2020-12-05 00:26:44 +0000

    Portfolio Fourth Commit
    v0.4 - Added button effects and groundwork for future js implementations

---
+++ assets/js/main.js
@@ -0,0 +1,61 @@
+// GoatBandit
+
+$ (document).ready (function() 
+{
+	// Front Page Buttons
+
+	// On About Button Click
+	$ ('.aboutButton').click (function (e) 
+	{
+	  $ ('.about').fadeIn (500);
+	  $ ('.aboutEffect').removeClass ('transform-out').addClass ('transform-in');
+  
+	  e.preventDefault();
+	});
+  
+		// On About Button Close Click
+		$ ('.about .inner .close').click (function (e) 
+		{
+		$ ('.about').fadeOut (500);
+		// JUST REMEMBER TO FIX THIS PROBLEM
+		$ ('.aboutEffect').removeClass ('transform-in').addClass ('transform-out');
+	
+		e.preventDefault();
+		});
+
+	// On Skills Button Click
+	$ ('.skillsButton').click (function (e) 
+	{
+	  $ ('.skills').fadeIn (500);
+	  $ ('.skillsEffect').removeClass ('transform-out').addClass ('transform-in');
+  
+	  e.preventDefault();
+	});
+  
+		// On Skills Button Close Click
+		$ ('.skills .inner .close').click (function (e) 
+		{
+		$ ('.skills').fadeOut (500);
+		$ ('.skillsEffect').removeClass ('transform-in').addClass ('transform-out');
+	
+		e.preventDefault();
+		});
+
+	// On Work Button Click
+	$ ('.workButton').click (function (e) 
+	{
+	  $ ('.work').fadeIn (500);
+	  $ ('.workEffect').removeClass ('transform-out').addClass ('transform-in');
+  
+	  e.preventDefault();
+	});
+  
+		// On Work Button Close Click
+		$ ('.work .inner .close').click (function (e) 
+		{
+		$ ('.work').fadeOut (500);
+		$ ('.workEffect').removeClass ('transform-in').addClass ('transform-out');
+	
+		e.preventDefault();
+		});
+  });
