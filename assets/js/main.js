43e1c3f5785c1836cda9bdeb6b1366df66ac294d
GoatBandit <connoreaster@gmail.com>
2020-12-07 00:20:49 +0000

    Portfolio Fifth Commit
    v0.5 - Finally adding depth to the website. I'm worried about possible wrapping issues on different devices but I have to commit to test that.
    
    Next commit should hopefully include a fully up-to-date CV and a boilerplate resume

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,55 +7,55 @@ $ (document).ready (function()
 	// On About Button Click
 	$ ('.aboutButton').click (function (e) 
 	{
-	  $ ('.about').fadeIn (500);
-	  $ ('.aboutEffect').removeClass ('transform-out').addClass ('transform-in');
-  
-	  e.preventDefault();
+		$ ('.about').fadeIn (500);
+		$ ('.aboutEffect').removeClass ('transform-out').addClass ('transform-in');
+	
+		e.preventDefault();
 	});
   
 		// On About Button Close Click
 		$ ('.about .inner .close').click (function (e) 
 		{
-		$ ('.about').fadeOut (500);
-		// JUST REMEMBER TO FIX THIS PROBLEM
-		$ ('.aboutEffect').removeClass ('transform-in').addClass ('transform-out');
-	
-		e.preventDefault();
+			$ ('.about').fadeOut (500);
+			// JUST REMEMBER TO FIX THIS PROBLEM
+			$ ('.aboutEffect').removeClass ('transform-in').addClass ('transform-out');
+		
+			e.preventDefault();
 		});
 
 	// On Skills Button Click
 	$ ('.skillsButton').click (function (e) 
 	{
-	  $ ('.skills').fadeIn (500);
-	  $ ('.skillsEffect').removeClass ('transform-out').addClass ('transform-in');
-  
-	  e.preventDefault();
+		$ ('.skills').fadeIn (500);
+		$ ('.skillsEffect').removeClass ('transform-out').addClass ('transform-in');
+	
+		e.preventDefault();
 	});
   
 		// On Skills Button Close Click
 		$ ('.skills .inner .close').click (function (e) 
 		{
-		$ ('.skills').fadeOut (500);
-		$ ('.skillsEffect').removeClass ('transform-in').addClass ('transform-out');
-	
-		e.preventDefault();
+			$ ('.skills').fadeOut (500);
+			$ ('.skillsEffect').removeClass ('transform-in').addClass ('transform-out');
+		
+			e.preventDefault();
 		});
 
 	// On Work Button Click
 	$ ('.workButton').click (function (e) 
 	{
-	  $ ('.work').fadeIn (500);
-	  $ ('.workEffect').removeClass ('transform-out').addClass ('transform-in');
-  
-	  e.preventDefault();
+		$ ('.work').fadeIn (500);
+		$ ('.workEffect').removeClass ('transform-out').addClass ('transform-in');
+	
+		e.preventDefault();
 	});
   
 		// On Work Button Close Click
 		$ ('.work .inner .close').click (function (e) 
 		{
-		$ ('.work').fadeOut (500);
-		$ ('.workEffect').removeClass ('transform-in').addClass ('transform-out');
-	
-		e.preventDefault();
+			$ ('.work').fadeOut (500);
+			$ ('.workEffect').removeClass ('transform-in').addClass ('transform-out');
+		
+			e.preventDefault();
 		});
-  });
+});
